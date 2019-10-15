7359f5d6a07d6dffdf07239a33dcfe51f34d58a9
bodhibrdr <julie.gumerman@gmail.com>
2019-10-15 15:09:55 -0600

    can add a new user

--- users/users-model.js
+++ users/users-model.js
@@ -1,8 +1,22 @@
-const db = "../data/dbconfig.js";
+const db = require("../data/dbconfig.js");
 
+const add = (user) => {
+    return db("users").insert(user, "id")
+        .then(ids => {
+            const [ id ] = ids;
+            return findById(id);
+        })
+}
 
+const findById = id => {
+    return db("users").where({id}).first();
+}
 
+const findBy = filter => {
+    return db("users").where(filter);
+}
 
 module.exports = {
-    
+    add,
+    findBy
 }
--- users/users-routes.js
+++ users/users-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcryptjs")
 const Users = require("./users-model");
 
 const userRouter = express.Router();
@@ -8,5 +9,22 @@ userRouter.get("/", (req, res) => {
     res.send("let the wild rumpus begin!")
 })
 
+userRouter.post("/register", (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+    Users.add(user)
+        .then(users => res.status(200).json(user))
+})
+
+userRouter.post("/login", (req, res) => {
+    let { username, password } = req.body;
+    Users.findBy({ username})
+        .first()
+        .then(user => {
+            res.status(200).json({ message: "Hey there!!!"})
+        })
+        .catch({ message: "We don't recognize your secret handshake."})
+})
 
 module.exports = userRouter;
